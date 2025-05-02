05998a6d3cdf8eb1f9f708b15081b67e7b81652c
sid446 <162982475+sid446@users.noreply.github.com>
2025-05-02 19:34:35 +0530

    first

--- src/index.js
+++ src/index.js
@@ -8,13 +8,13 @@ dotenv.config(
     }
 );
 
-connectDB()
-.then(() => {
-    app.listen(process.env.PORT, () => {
-        console.log(`Server running on port ${process.env.PORT}`);
-    });
-})
-.catch((error) => {
-    console.error(`Error: ${error.message}`);
-    process.exit(1);
-});
+connectDB().
+    then(()=>{
+        app.listen(process.env.PORT,()=>{
+            console.log(`Server is running on port ${process.env.PORT}`);
+        })
+    })
+    .catch((error)=>{
+        console.error(`Error: ${error.message}`);
+    })
+
